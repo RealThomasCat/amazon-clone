26c26a392cd2106c70551647ba44df1073460285
Yashveer Singh <yashveer.rtc@gmail.com>
2022-10-01 12:13:02 +0530

    user signin signup compelete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
@@ -9,13 +10,17 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
-        
+
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCDCWrwliDPimOOVQtuj2gTCvJ3h_nWmb4",
-    authDomain: "challenge-789ed.firebaseapp.com",
-    projectId: "challenge-789ed",
-    storageBucket: "challenge-789ed.appspot.com",
-    messagingSenderId: "109276200582",
-    appId: "1:109276200582:web:806a38a7584070054c1b07",
-    measurementId: "G-755RVRKFWG"
-  };
+  apiKey: "AIzaSyCDCWrwliDPimOOVQtuj2gTCvJ3h_nWmb4",
+  authDomain: "challenge-789ed.firebaseapp.com",
+  projectId: "challenge-789ed",
+  storageBucket: "challenge-789ed.appspot.com",
+  messagingSenderId: "109276200582",
+  appId: "1:109276200582:web:806a38a7584070054c1b07",
+  measurementId: "G-755RVRKFWG"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+export { db, auth };
